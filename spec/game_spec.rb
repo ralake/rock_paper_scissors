04b2bcc540a17feb7ed25c292fff6eea101d144e
Richard Lake <ralake1985@gmail.com>
2014-11-30 13:23:12 +0000

    Add more tests for winning and losing scenarios.

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,33 +2,61 @@ require 'spec_helper'
 
 describe Game do
 
-  let(:hal) { double :ComputerPlayer, choice: "Scissors" }
-  let(:hal2) { double :ComputerPlayer, choice: "Paper" }
-  let(:hal3) { double :ComputerPlayer, choice: "Rock" }
-  let(:rich) { double :Player , choice: "Rock" }
-  let(:game) { Game.new }
-
-  before(:each) do
-    game.add_player(rich)
-  end
+  let(:hal)         { double :ComputerPlayer, choice: "Scissors" }
+  let(:droid)       { double :ComputerPlayer, choice: "Paper" }
+  let(:johnny_five) { double :ComputerPlayer, choice: "Rock" }
+  let(:rich)        { double :Player , choice: "Rock" }
+  let(:clare)       { double :Player , choice: "Scissors" }
+  let(:will)       { double :Player , choice: "Paper" }
+  let(:game)        { Game.new }
 
   it 'can add a player' do
+    game.add_player(rich)
     expect(game.players).to eq([rich])
   end
 
-  it 'knows if a player has beaten the computer' do
+  it 'knows that rock beats scissors' do
+    game.add_player(rich)
     game.add_player(hal)
-    expect(game.results).to eq("You win!")
+    expect(game.results).to eq('You win!')
   end
 
-  it 'knows if a player has been beaten by the computer' do
-    game.add_player(hal2)
-    expect(game.results).to eq("You lose...")
+  it 'knows that scissors beats paper' do
+    game.add_player(clare)
+    game.add_player(droid)
+    expect(game.results).to eq('You win!')
+  end
+
+  it 'knows that paper beats rock' do
+    game.add_player(will)
+    game.add_player(johnny_five)
+    expect(game.results).to eq('You win!')
+  end
+
+  it 'knows that scissors lose to rock' do
+    game.add_player(clare)
+    game.add_player(johnny_five)
+    expect(game.results).to eq('You lose...')
+  end
+
+  it 'knows that paper loses to scissors' do
+    game.add_player(will)
+    game.add_player(hal)
+    expect(game.results).to eq('You lose...')
+  end
+
+  it 'knows that rock loses to paper' do
+    game.add_player(rich)
+    game.add_player(droid)
+    expect(game.results).to eq('You lose...')
   end
 
   it 'knows if the game is a draw' do
-    game.add_player(hal3)
+    game.add_player(rich)
+    game.add_player(johnny_five)
     expect(game.results).to eq("It's a draw.")
   end
   
+
+
 end
